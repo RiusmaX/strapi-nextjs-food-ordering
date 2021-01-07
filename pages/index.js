6a42f066ba2239a476d8d9e43062e76032fee08c
Marius <marius.sergent2014@campus-eni.fr>
2021-01-07 13:42:54 +0100

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Button, Alert } from 'reactstrap'
+
+export default () => {
+  return (
+    <div>
+      <div>
+        <Alert color='primary'>Super Projet avec nextJS et Bootstrap</Alert>
+        &nbsp; <Button color='primary'>Hello from nextjs</Button>
+      </div>
+    </div>
+  )
+}
